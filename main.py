1bc794b10f17a42fdfd4e1045e284842143f590d
N-MaheAlam <nmahealam@gmail.com>
2023-12-13 09:05:27 -0500

    Strings Manipulations

---
+++ main.py
@@ -0,0 +1,22 @@
+
+print("STRING MANIPULATION: ")
+
+print("Nazmul Mahe Alam")
+
+
+# print the "print" in console
+print("print('what to print')")
+
+# Printing each name in new line
+print("Nazmul\nMahe\nAlam \n Space")
+
+# Without any space
+print("Hello" + "Kumu")
+
+# Printing in single quote and give a space before "Kumu"
+print('Hello' +' Kumu')
+
+# ERROR
+# IndentationError = happens when we put any unexpected space or TAB
+# Syntax error = misspell of any syntax
+

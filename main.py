72c28b72509b05a8da281c47c581e8a397a86025
N-MaheAlam <nmahealam@gmail.com>
2023-12-17 14:26:41 -0500

    Leap Year project Added

--- main.py
+++ main.py
@@ -267,3 +267,18 @@ elif 30 <= bmi < 35:
     print(f"Your BMI is {bmi}, you are obese.")
 else:
     print(f"Your BMI is {bmi}, you are clinically obese.")
+print("============ Nested else/if Leap Year : Practice 16 ============")
+
+year = int(input())
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not leap year")
+    else:
+        print("Leap year")
+
+else:
+    print("Not leap year")
